eb8825e27c755b15b0065f6ed081c4a5b5e26a49
Harwin14 <agungharwinn@gmail.com>
2023-04-28 22:04:31 +0700

    done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
                                 </div>
                             </form>
                         </div>
-                    </div> 
+                    </div>   
                 </div>
             </div>
         </section>
